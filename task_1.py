dd83b9f805f2700c40784fd25f244ecefe082ee1
TomaVitalina <86037217+TomaVitalina@users.noreply.github.com>
2024-04-19 14:14:37 +0300

    3

---
+++ task_1.py
@@ -0,0 +1,34 @@
+# Aceasta este prima ta sarcină legată de variabile în python
+# Creează o variabilă numită `age` și seteaz-o la vârsta ta
+
+# CODUL TĂU VINE MAI JOS:
+age=45
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează valoarea variabilei `age` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(age)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum schimbă valoarea variabilei `age` la vârsta prietenului tău și afișează valoarea variabilei `age` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+age=50
+print(age)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creează o nouă variabilă numită `age2` și seteaz-o la valoarea variabilei `age` și afișează valoarea variabilei `age2` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+age2=age
+# CODUL TĂU VINE MAI SUS:
+
+# Acum șterge variabila `age` și încearcă să afișezi valoarea variabilei `age` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+del(age)
+print(age)
+# CODUL TĂU VINE MAI SUS:
+
+# Asta a fost tot pentru această sarcină
